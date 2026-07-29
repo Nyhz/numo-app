337788ca1f8df1b26daef848fb4e5d0606da8260
DEVROOM @ Nyhz <nyhz@devroom.local>
2026-07-29 21:39:59 +0200

    revert(perf): retirar loading.tsx por ruta y PageSkeleton

--- src/app/settings/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { PageSkeleton } from "@/src/components/ui/PageSkeleton";
-
-export default function Loading() {
-  return <PageSkeleton blocks={["table", "table"]} />;
-}
--- src/app/taxes/[year]/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { PageSkeleton } from "@/src/components/ui/PageSkeleton";
-
-export default function Loading() {
-  return <PageSkeleton kpiCells={3} blocks={["table", "table"]} />;
-}
